a255e144de6a4feb76a74e53816e46e7759689f0
IMRC21 <marianoradu@mail.com>
2018-07-23 08:31:28 +0200

    Uploaded 0442 for testing

---
+++ 04421_test/js/scripts.js
@@ -0,0 +1,50 @@
+var menuIsOpen;
+
+function openNav() {
+    document.getElementById("left-menu").style.width = "80%";
+	$("#left-menu").css("display","block");
+
+}
+
+function closeNav() {
+    document.getElementById("left-menu").style.width = "0";
+		$("#left-menu").css("display","none");
+
+}
+
+$(document).ready(function()
+{
+	/*menuIsOpen = false;
+	$("#open-menu").click(function(){
+		$(".push-left").css("display", "block");
+		//$("#container").css("filter", "blur(5px)");
+	});
+	
+	
+	$("#container").click(function(){
+	if($(".push-left").css("display") == "block"){
+		$(".push-left").css("display", "none");
+		menuIsOpen = false;
+	}
+	});
+*/
+	if ($(window).width() <= 600) {  
+			$("#mobile").show();
+			$("#menu").hide();
+		   }else{
+			$("#mobile").hide();
+		 	$("#menu").show();
+		   }
+	//if the window is resized I do the same thing
+	$(window).resize(function(){
+		$("#mobile").hide();
+		   if ($(window).width() <= 600) {  
+			   $("#mobile").show();
+			   $("#menu").hide();
+		   }else{
+			   $("#mobile").hide();
+			   $("#menu").show();
+		   }     
+
+	});
+});
